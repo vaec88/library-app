cac72749c1684ea53887e1f7b1c72cf7cc061e5a
Víctor Eras <victoreras88@hotmail.com>
2026-09-11 16:29:34 -0500

    add audit fields

--- library-app-backend/src/main/resources/database/schema.sql
+++ library-app-backend/src/main/resources/database/schema.sql
@@ -2,7 +2,9 @@ CREATE TABLE category (
     id          SERIAL PRIMARY KEY,
     name        VARCHAR(100) NOT NULL,
     description VARCHAR(255),
-    status      BOOLEAN NOT NULL DEFAULT TRUE
+    status      BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE book (
@@ -10,8 +12,10 @@ CREATE TABLE book (
     title       VARCHAR(200) NOT NULL,
     author      VARCHAR(150) NOT NULL,
     isbn        VARCHAR(13) UNIQUE,
-    available   BOOLEAN NOT NULL DEFAULT TRUE,
-    category_id INT NOT NULL REFERENCES category(id)
+    available   BOOLEAN NOT NULL DEFAULT TRUE
+    category_id INT NOT NULL REFERENCES category(id),
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE client (
@@ -19,17 +23,23 @@ CREATE TABLE client (
     first_name VARCHAR(100) NOT NULL,
     last_name  VARCHAR(100) NOT NULL,
     id_number  VARCHAR(10) UNIQUE NOT NULL,
-    email      VARCHAR(150) UNIQUE NOT NULL
+    email      VARCHAR(150) UNIQUE NOT NULL,
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE reservation (
     id                SERIAL PRIMARY KEY,
-    reservation_date  DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    client_id         INT NOT NULL REFERENCES client(id)
+    reservation_date  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    client_id         INT NOT NULL REFERENCES client(id),
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE reservation_detail (
     id             SERIAL PRIMARY KEY,
     reservation_id INT NOT NULL REFERENCES reservation(id),
-    book_id        INT NOT NULL REFERENCES book(id)
+    book_id        INT NOT NULL REFERENCES book(id),
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
