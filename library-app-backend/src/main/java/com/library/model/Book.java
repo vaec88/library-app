20ccf9588fd21cb5bb68f143826112cc35ab41f7
Víctor Eras <victoreras88@hotmail.com>
2026-09-19 12:19:03 -0500

    fix N+1 problem

--- library-app-backend/src/main/java/com/library/model/Book.java
+++ library-app-backend/src/main/java/com/library/model/Book.java
@@ -2,6 +2,7 @@ package com.library.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -37,7 +38,7 @@ public class Book extends BaseEntity {
     @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT TRUE")
     private Boolean available;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
 
--- library-app-backend/src/main/java/com/library/repository/IBookRepository.java
+++ library-app-backend/src/main/java/com/library/repository/IBookRepository.java
@@ -1,8 +1,14 @@
 package com.library.repository;
 
 import com.library.model.Book;
+import org.springframework.data.jpa.repository.EntityGraph;
+
+import java.util.List;
 
 public interface IBookRepository extends IGenericRepository<Book, Integer> {
+
+    @EntityGraph(attributePaths = "category")
+    List<Book> findAll();
 
     boolean existsByCategoryId(Integer id);
 }
